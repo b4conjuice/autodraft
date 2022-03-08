b6abf6300fd66c2eb03bc0d77102f65ac337b102
Daniel Lopez <dlopez807@gmail.com>
2022-03-08 15:56:52 -0800

    remove comments

--- src/components/page.js
+++ src/components/page.js
@@ -21,64 +21,10 @@ const Page = ({ title = DEFAULT_TITLE, children }) => {
         <meta name='description' content='autodraft' />
         <meta name='theme-color' content='#3b5998' />
         <link rel='shortcut icon' href='/favicon.png' />
-        {/* <link rel="apple-touch-icon" sizes="57x57" href="/logo-57x57.png" />
-      <link rel="apple-touch-icon" sizes="72x72" href="/logo-72x72.png" />
-      <link rel="apple-touch-icon" sizes="114x114" href="/logo-114x114.png" />
-      <link rel="apple-touch-icon" sizes="144x144" href="/logo-144x144.png" /> */}
         <link rel='apple-touch-icon' sizes='57x57' href='/icon.png' />
         <link rel='apple-touch-icon' sizes='72x72' href='/icon.png' />
         <link rel='apple-touch-icon' sizes='114x114' href='/icon.png' />
         <link rel='apple-touch-icon' sizes='144x144' href='/icon.png' />
-        {/* <link
-        href="splashscreens/iphone5_splash.png"
-        media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)"
-        rel="apple-touch-startup-image"
-      />
-      <link
-        href="splashscreens/iphone6_splash.png"
-        media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
-        rel="apple-touch-startup-image"
-      />
-      <link
-        href="splashscreens/iphoneplus_splash.png"
-        media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)"
-        rel="apple-touch-startup-image"
-      />
-      <link
-        href="splashscreens/iphonex_splash.png"
-        media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
-        rel="apple-touch-startup-image"
-      />
-      <link
-        href="splashscreens/iphonexr_splash.png"
-        media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)"
-        rel="apple-touch-startup-image"
-      />
-      <link
-        href="splashscreens/iphonexsmax_splash.png"
-        media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
-        rel="apple-touch-startup-image"
-      />
-      <link
-        href="splashscreens/ipad_splash.png"
-        media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)"
-        rel="apple-touch-startup-image"
-      />
-      <link
-        href="splashscreens/ipadpro1_splash.png"
-        media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)"
-        rel="apple-touch-startup-image"
-      />
-      <link
-        href="splashscreens/ipadpro3_splash.png"
-        media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)"
-        rel="apple-touch-startup-image"
-      />
-      <link
-        href="splashscreens/ipadpro2_splash.png"
-        media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)"
-        rel="apple-touch-startup-image"
-      /> */}
         <title>
           {title === DEFAULT_TITLE ? title : `${title} - ${DEFAULT_TITLE}`}
         </title>
