85ed94ca5cb556f893d0fbfc1c66f6b32cf29fe8
Daniel Lopez <dlopez807@gmail.com>
2023-10-31 19:28:24 -0700

    fix min in box scores on team pages

--- src/lib/getMin.js
+++ src/lib/getMin.js
@@ -2,13 +2,15 @@ const getMin = min => {
   if (!min || min === '0') return 0
 
   // * looks like min stat on balldontlie's api was changed to be without `:seconds`
-  // if (!min.includes(':')) {
-  //   console.error(`min ${min} is not the correct time format`)
-  //   return 0
-  // }
-  // const [m, s] = min.split(':')
-  // return parseInt(m, 10) * 60 + parseInt(s, 10)
-  return Number(min)
+  if (!min.includes(':')) {
+    // console.error(`min ${min} is not the correct time format`)
+    // return 0
+
+    // assume that min is literally just minutes
+    return Number(min)
+  }
+  const [m, s] = min.split(':')
+  return parseInt(m, 10) * 60 + parseInt(s, 10)
 }
 
 export default getMin
