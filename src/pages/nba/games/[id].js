d51f4779cae20745b7069fa0abffdfe3d1bc5bdd
Daniel Lopez <dlopez807@gmail.com>
2023-11-03 19:03:46 -0700

    add toggle to switch box score of teams

--- src/pages/nba/games/[id].js
+++ src/pages/nba/games/[id].js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 
@@ -11,6 +12,7 @@ import Layout from '@/components/layout'
 
 const Game = () => {
   const { query } = useRouter()
+  const [showAwayTeam, setShowAwayTeam] = useState(true)
   const { id } = query
   const { data: game, error } = fetchNBAGame(id)
   if (error || !game || !game.stats)
@@ -25,7 +27,7 @@ const Game = () => {
     >
       <Layout>
         <Main>
-          <div className='mx-auto space-y-2 md:max-w-screen-md'>
+          <div className='mx-auto space-y-2 md:max-w-screen-lg'>
             <div className='text-center'>
               <h1 className='space-x-2 text-2xl'>
                 <span className='space-x-1'>
@@ -71,18 +73,64 @@ const Game = () => {
                     />
                   </div>
                 </div>
-                <BoxScore
-                  stats={game.stats.filter(
-                    s => s.team.id === game.visitor_team.id
+                <div className='hidden space-y-2 md:block'>
+                  <BoxScore
+                    stats={game.stats.filter(
+                      s => s.team.id === game.visitor_team.id
+                    )}
+                    includeTotals
+                  />
+                  <BoxScore
+                    stats={game.stats.filter(
+                      s => s.team.id === game.home_team.id
+                    )}
+                    includeTotals
+                  />
+                </div>
+                <div className='block space-y-2 md:hidden'>
+                  <label
+                    htmlFor='checked'
+                    className='inline-flex cursor-pointer items-center justify-center space-x-3 divide-skin-foreground'
+                  >
+                    <span className={!showAwayTeam ? 'text-gray-500' : ''}>
+                      {game.visitor_team.abbreviation}
+                    </span>
+                    <span className='relative'>
+                      <span className='block h-6 w-10 rounded-full bg-gray-400 shadow-inner' />
+                      <span
+                        className={`focus-within:shadow-outline absolute inset-y-0 left-0 mt-1 ml-1 block h-4 w-4 rounded-full bg-skin-button-accent shadow transition-transform duration-300 ease-in-out ${
+                          !showAwayTeam ? 'translate-x-full transform' : ''
+                        }`}
+                      >
+                        <input
+                          id='checked'
+                          type='checkbox'
+                          checked
+                          onChange={() => setShowAwayTeam(!showAwayTeam)}
+                          className='absolute h-0 w-0 opacity-0'
+                        />
+                      </span>
+                    </span>
+                    <span className={showAwayTeam ? 'text-gray-500' : ''}>
+                      {game.home_team.abbreviation}
+                    </span>
+                  </label>
+                  {showAwayTeam ? (
+                    <BoxScore
+                      stats={game.stats.filter(
+                        s => s.team.id === game.visitor_team.id
+                      )}
+                      includeTotals
+                    />
+                  ) : (
+                    <BoxScore
+                      stats={game.stats.filter(
+                        s => s.team.id === game.home_team.id
+                      )}
+                      includeTotals
+                    />
                   )}
-                  includeTotals
-                />
-                <BoxScore
-                  stats={game.stats.filter(
-                    s => s.team.id === game.home_team.id
-                  )}
-                  includeTotals
-                />
+                </div>
               </>
             ) : null}
           </div>
